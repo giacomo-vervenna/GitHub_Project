4178ed246305c85c126c750091a5c1739049e18f
giacomo-vervenna <giacomo.vervenna@aesys.tech>
2022-11-02 17:50:03 +0100

    progetto pasticceria

---
+++ Pasticceria_Interattiva/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author jackie
+ *
+ */
+module Pasticceria_Interattiva {
+}
---
+++ Pasticceria_Interattiva/src/src/Cake.java
@@ -0,0 +1,26 @@
+package src;
+public class Cake {
+
+	private String name;
+	private double price;
+
+	public Cake() {
+
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+}
